5af75589654642ce882bec431b31b7be46d6b924
Snehal Karwa <snehalkarwa77@gmail.com>
2022-01-31 00:21:49 -0500

    first_commit

---
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -0,0 +1,53 @@
+package com.mindex.challenge.data;
+
+import java.time.LocalDate;
+
+/**
+ * This is the class that defines the Compensation type
+ * @author     Snehal Karwa
+ */
+public class Compensation {
+    private Employee employee;
+    private double salary;
+    private LocalDate effectiveDate;
+
+    public Compensation() {
+    }
+
+    public Compensation(Employee employee, double salary, LocalDate effectiveDate) {
+        this.employee = employee;
+        this.salary = salary; 
+        this.effectiveDate = effectiveDate;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Compensation{" + "employee =" + employee + ", salary =" + salary +
+                ", effectiveDate =" + effectiveDate + '}';
+    }
+
+}
